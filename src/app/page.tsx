3c9ff8948bd634718cc01db09b694333d5db14c4
Muchammad Ardiansyah <achardianself@gmail.com>
2026-03-07 06:31:09 +0700

    chore: initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@heroui/button';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold text-red-500 underline'>Hello there</h1>
+      <Button color='primary'>Click here</Button>
+    </div>
+  );
+};
+
+export default HomePage;
